fa007574b97fab4585f51f5e14f903a1e8cd2bdb
Jonathan Medrano <jonathanmedranoo41@gmail.com>
2025-07-05 11:18:29 -0300

    Commit one

---
+++ muchomasgestor/src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
+import "primereact/resources/themes/lara-light-blue/theme.css"; // o cualquier otro tema
+import "primereact/resources/primereact.min.css";
+import "primeicons/primeicons.css";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>
+);
